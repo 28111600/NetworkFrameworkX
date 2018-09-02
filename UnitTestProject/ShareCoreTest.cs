47a0f55bb0fd24fbfe4c051166f53d962643ce47
xiaodong <28111600@qq.com>
2018-09-02 21:22:54 +0800

    增加单元测试

--- UnitTestProject/ShareCoreTest.cs
+++ UnitTestProject/ShareCoreTest.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NetworkFrameworkX.Share;
 
@@ -8,15 +11,75 @@ namespace NetworkFrameworkX.UnitTestProject
     [TestClass]
     public class ShareCoreTest
     {
+        [DataContract]
+        private class TestObject
+        {
+            [DataMember]
+            public string String { get; set; }
+
+            [DataMember]
+            public int Int { get; set; }
+
+            [DataMember]
+            public long Long { get; set; }
+
+            [DataMember]
+            public byte[] Bytes { get; set; }
+
+            static public TestObject Generater()
+            {
+                TestObject result = new TestObject()
+                {
+                    String = "String",
+                    Int = int.MaxValue,
+                    Long = long.MaxValue,
+                    Bytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }
+                };
+
+                return result;
+            }
+
+            static public bool IsEquals(TestObject a, TestObject b)
+            {
+                return a.String.Equals(b.String) && a.Int== b.Int && a.Long==b.Long && Enumerable.SequenceEqual(a.Bytes,b.Bytes);
+            }
+        }
+
         [TestMethod]
         public void JsonSerialzationTest()
         {
-            var jsonSerialzation = new JsonSerialzation();
-            byte[] input = Guid.NewGuid().ToByteArray();
-            byte[] output = jsonSerialzation.Deserialize<byte[]>(jsonSerialzation.Serialize(input));
-            bool value = Enumerable.SequenceEqual(input, output);
+            var serialzer = new JsonSerialzation();
+            TestObject input = TestObject.Generater();
+            TestObject output = serialzer.Deserialize<TestObject>(serialzer.Serialize(input));
+            bool value = TestObject.IsEquals(input, output);
 
             Assert.IsTrue(value);
+        }
+
+        [TestMethod]
+        public void DataContractJsonSerializerTest()
+        {
+            var serialzer = new DataContractJsonSerializer(typeof(TestObject));
+            TestObject input = TestObject.Generater();
+
+            using (MemoryStream msr = new MemoryStream()) {
+                serialzer.WriteObject(msr, input);
+                msr.Flush();
+                msr.Seek(0, SeekOrigin.Begin);
+                StreamReader sr = new StreamReader(msr);
+                string s = sr.ReadToEnd();
+
+                using (MemoryStream msw = new MemoryStream()) {
+                    StreamWriter sw = new StreamWriter(msw);
+                    sw.Write(s);
+                    sw.Flush();
+                    msw.Seek(0, SeekOrigin.Begin);
+                    TestObject output = (TestObject)serialzer.ReadObject(msw);
+                    bool value = TestObject.IsEquals(input, output);
+
+                    Assert.IsTrue(value);
+                }
+            }
         }
 
         [TestMethod]
