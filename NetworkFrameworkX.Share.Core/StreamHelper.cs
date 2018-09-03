ba224a9281f0f7e204500e893fccdb44cbcd156a
xiaodong <28111600@qq.com>
2018-09-03 21:20:35 +0800

    优化抛出异常

--- NetworkFrameworkX.Share.Core/StreamHelper.cs
+++ NetworkFrameworkX.Share.Core/StreamHelper.cs
@@ -73,14 +73,14 @@ namespace NetworkFrameworkX.Share
                 byte[] data = buffer.Take(readBufferLength);
 
                 while (data != null && data.Length > 0) {
-                    if (this.lengthOfPacket > this.MaxSizeOfPacket || this.lengthOfPacket < 0) {
-                        // 非法长度，抛出异常
-                        throw new Exception(ERR_HEAP_CORRUPTION);
-                    }
-
                     if (this.lengthOfPacket == 0) {
                         // 从头开始读取
                         this.lengthOfPacket = BitConverter.ToInt32(data.Take(LENGTH_OF_HEAD), 0);
+
+                        if (this.lengthOfPacket > this.MaxSizeOfPacket || this.lengthOfPacket <= 0) {
+                            // 非法长度，抛出异常
+                            throw new Exception(ERR_HEAP_CORRUPTION);
+                        }
 
                         this.indexOfPacket = 0;
                         this.bufferOfPacket = new byte[this.lengthOfPacket];
